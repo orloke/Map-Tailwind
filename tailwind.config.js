744fbf673b8e2a5f1ad841ac51967f047f8cf071
Junior Dering <juniordering@hotmail.com>
2022-12-12 10:09:45 -0400

    correcting name

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/Components/**/*.{js,ts,jsx,tsx}',
     './src/Templates/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
